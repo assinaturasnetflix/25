6e8983e2634c5443e5a419391d18e301ad2eeebb
assinaturasnetflix <jonaspedroqt@gmail.com>
2025-07-14 16:32:29 +0200

    Add files via upload

--- gameLogic.js
+++ gameLogic.js
@@ -6,143 +6,62 @@ const PIECES = {
     P2_KING: 4,
 };
 
+const isP1 = (p) => p === PIECES.P1_PAWN || p === PIECES.P1_KING;
+const isP2 = (p) => p === PIECES.P2_PAWN || p === PIECES.P2_KING;
+const isKing = (p) => p === PIECES.P1_KING || p === PIECES.P2_KING;
+const isValid = (r, c) => r >= 0 && r < 8 && c >= 0 && c < 8;
+const isSamePlayer = (p1, p2) => (isP1(p1) === isP1(p2)) && p1 !== PIECES.EMPTY && p2 !== PIECES.EMPTY;
+
 const createInitialBoard = () => {
-    const board = Array(8).fill(null).map(() => Array(8).fill(PIECES.EMPTY));
-    for (let row = 0; row < 3; row++) {
-        for (let col = 0; col < 8; col++) {
-            if ((row + col) % 2 !== 0) {
-                board[row][col] = PIECES.P2_PAWN;
-            }
-        }
-    }
-    for (let row = 5; row < 8; row++) {
-        for (let col = 0; col < 8; col++) {
-            if ((row + col) % 2 !== 0) {
-                board[row][col] = PIECES.P1_PAWN;
-            }
-        }
-    }
-    return board;
+    return [
+        [0,2,0,2,0,2,0,2],[2,0,2,0,2,0,2,0],[0,2,0,2,0,2,0,2],
+        [0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],
+        [1,0,1,0,1,0,1,0],[0,1,0,1,0,1,0,1],[1,0,1,0,1,0,1,0]
+    ];
 };
 
-const isPlayerPiece = (piece, player) => {
-    if (player === 1) return piece === PIECES.P1_PAWN || piece === PIECES.P1_KING;
-    if (player === 2) return piece === PIECES.P2_PAWN || piece === PIECES.P2_KING;
-    return false;
-};
+const getPieceMoves = (board, r, c) => {
+    const type = board[r][c];
+    if (type === PIECES.EMPTY) return { captures: [], simple: [] };
 
-const findCaptureSequences = (board, player) => {
-    let allSequences = [];
+    let moves = { captures: [], simple: [] };
+    const fwd = isP1(type) ? -1 : 1;
+    const dirs = [[-1,-1], [-1,1], [1,-1], [1,1]];
 
-    const findJumps = (currentBoard, r, c, currentPath) => {
-        const piece = currentBoard[r][c];
-        const isKing = piece === PIECES.P1_KING || piece === PIECES.P2_KING;
-        let foundJump = false;
+    for (const [dr, dc] of dirs) {
+        let path = [];
+        let foundOpponent = null;
+        for (let i = 1; i < 8; i++) {
+            const cr = r + i * dr, cc = c + i * dc;
+            if (!isValid(cr, cc)) break;
+            
+            const cellType = board[cr][cc];
 
-        const directions = isKing ? 
-            [[-1, -1], [-1, 1], [1, -1], [1, 1]] :
-            [[-1, -1], [-1, 1], [1, -1], [1, 1]];
-
-        for (const [dr, dc] of directions) {
-            let path = [];
-            let lastR = r, lastC = c;
-
-            if (isKing) {
-                 for (let i = 1; i < 8; i++) {
-                    const middleR = r + i * dr, middleC = c + i * dc;
-                    const landR = r + (i + 1) * dr, landC = c + (i + 1) * dc;
-
-                    if (landR < 0 || landR >= 8 || landC < 0 || landC >= 8) break;
-                    
-                    const middlePiece = currentBoard[middleR][middleC];
-                    const landPiece = currentBoard[landR][landC];
-
-                    if (middlePiece !== PIECES.EMPTY && !isPlayerPiece(middlePiece, player)) {
-                        if (landPiece === PIECES.EMPTY) {
-                            const newBoard = JSON.parse(JSON.stringify(currentBoard));
-                            newBoard[landR][landC] = newBoard[r][c];
-                            newBoard[r][c] = PIECES.EMPTY;
-                            newBoard[middleR][middleC] = PIECES.EMPTY;
-                            foundJump = true;
-                            findJumps(newBoard, landR, landC, [...currentPath, [landR, landC]]);
-                        }
-                        break;
-                    }
-                     if (middlePiece !== PIECES.EMPTY) break;
-                }
+            if (foundOpponent) {
+                if (cellType === PIECES.EMPTY) {
+                    moves.captures.push([[r,c], [cr,cc]]);
+                } else break;
             } else {
-                const middleR = r + dr, middleC = c + dc;
-                const landR = r + 2 * dr, landC = c + 2 * dc;
-                
-                if (landR >= 0 && landR < 8 && landC >= 0 && landC < 8) {
-                    const middlePiece = currentBoard[middleR][middleC];
-                    const landPiece = currentBoard[landR][landC];
-
-                    if (landPiece === PIECES.EMPTY && middlePiece !== PIECES.EMPTY && !isPlayerPiece(middlePiece, player)) {
-                        const newBoard = JSON.parse(JSON.stringify(currentBoard));
-                        newBoard[landR][landC] = newBoard[r][c];
-                        newBoard[r][c] = PIECES.EMPTY;
-                        newBoard[middleR][middleC] = PIECES.EMPTY;
-                        foundJump = true;
-                        findJumps(newBoard, landR, landC, [...currentPath, [landR, landC]]);
-                    }
-                }
-            }
-        }
-        
-        if (!foundJump && currentPath.length > 1) {
-            allSequences.push(currentPath);
-        }
-    };
-
-    for (let r = 0; r < 8; r++) {
-        for (let c = 0; c < 8; c++) {
-            if (isPlayerPiece(board[r][c], player)) {
-                findJumps(board, r, c, [[r, c]]);
-            }
-        }
-    }
-    
-    if (allSequences.length === 0) return [];
-    
-    let maxLength = 0;
-    for (const seq of allSequences) {
-        if (seq.length > maxLength) {
-            maxLength = seq.length;
-        }
-    }
-    
-    return allSequences.filter(seq => seq.length === maxLength);
-};
-
-
-const findSimpleMoves = (board, player) => {
-    const moves = [];
-    for (let r = 0; r < 8; r++) {
-        for (let c = 0; c < 8; c++) {
-            if (!isPlayerPiece(board[r][c], player)) continue;
-
-            const piece = board[r][c];
-            const isKing = piece === PIECES.P1_KING || piece === PIECES.P2_KING;
-            const pawnDir = (player === 1) ? -1 : 1;
-            const directions = isKing ? [[-1, -1], [-1, 1], [1, -1], [1, 1]] : [[pawnDir, -1], [pawnDir, 1]];
-
-            for (const [dr, dc] of directions) {
-                if(isKing){
-                    for(let i=1; i < 8; i++){
-                        const newR = r + i * dr, newC = c + i * dc;
-                        if (newR >= 0 && newR < 8 && newC >= 0 && newC < 8 && board[newR][newC] === PIECES.EMPTY) {
-                            moves.push([[r, c], [newR, newC]]);
-                        } else {
-                            break;
-                        }
-                    }
+                if (cellType === PIECES.EMPTY) {
+                    path.push([[r,c], [cr,cc]]);
                 } else {
-                    const newR = r + dr, newC = c + dc;
-                    if (newR >= 0 && newR < 8 && newC >= 0 && newC < 8 && board[newR][newC] === PIECES.EMPTY) {
-                        moves.push([[r, c], [newR, newC]]);
-                    }
+                    if (!isSamePlayer(type, cellType)) {
+                        const lr = cr + dr, lc = cc + dc;
+                        if (isValid(lr, lc) && board[lr][lc] === PIECES.EMPTY) {
+                            foundOpponent = {r: cr, c: cc};
+                            i++; // Skip opponent's square on next iteration
+                            moves.captures.push([[r,c], [lr,lc]]);
+                        } else break;
+                    } else break;
                 }
+            }
+            if (!isKing(type) && !foundOpponent) break;
+        }
+        if (!foundOpponent) {
+            if (isKing(type)) {
+                moves.simple.push(...path);
+            } else if (path.length > 0 && (path[0][1][0] - r) === fwd) {
+                moves.simple.push(path[0]);
             }
         }
     }
@@ -150,51 +69,74 @@ const findSimpleMoves = (board, player) => {
 };
 
 const getValidMoves = (board, player) => {
-    const captures = findCaptureSequences(board, player);
-    if (captures.length > 0) {
-        return captures;
+    let allCaptures = [];
+    let allSimpleMoves = [];
+    let maxCaptureLength = 0;
+
+    for (let r = 0; r < 8; r++) {
+        for (let c = 0; c < 8; c++) {
+            const type = board[r][c];
+            if (type === PIECES.EMPTY || (player === 1 && !isP1(type)) || (player === 2 && !isP2(type))) continue;
+
+            const findSequences = (currentBoard, startPos, currentPath) => {
+                const moves = getPieceMoves(currentBoard, startPos[0], startPos[1]).captures;
+                if (moves.length === 0) {
+                    if(currentPath.length > 1) {
+                         allCaptures.push(currentPath);
+                         maxCaptureLength = Math.max(maxCaptureLength, currentPath.length - 1);
+                    }
+                    return;
+                }
+                moves.forEach(move => {
+                    const newBoard = applyMove(currentBoard, move);
+                    findSequences(newBoard, move[1], [...currentPath, move[1]]);
+                });
+            };
+            
+            findSequences(JSON.parse(JSON.stringify(board)), [r, c], [[r, c]]);
+            
+            if(allCaptures.length === 0){
+                allSimpleMoves.push(...getPieceMoves(board, r, c).simple);
+            }
+        }
     }
-    return findSimpleMoves(board, player);
+
+    if (allCaptures.length > 0) {
+        return allCaptures.filter(seq => (seq.length - 1) === maxCaptureLength);
+    }
+    return allSimpleMoves;
 };
 
 const applyMove = (board, moveSequence) => {
     const newBoard = JSON.parse(JSON.stringify(board));
     const startPos = moveSequence[0];
     const endPos = moveSequence[moveSequence.length - 1];
-    const piece = newBoard[startPos[0]][startPos[1]];
+    let piece = newBoard[startPos[0]][startPos[1]];
 
     newBoard[startPos[0]][startPos[1]] = PIECES.EMPTY;
 
-    if (moveSequence.length > 2) { 
-        for (let i = 0; i < moveSequence.length - 1; i++) {
-            const current = moveSequence[i];
-            const next = moveSequence[i+1];
-            const dr = Math.sign(next[0] - current[0]);
-            const dc = Math.sign(next[1] - current[1]);
-            
-            let r = current[0] + dr;
-            let c = current[1] + dc;
-
-            while (r !== next[0] || c !== next[1]) {
-                if(newBoard[r][c] !== PIECES.EMPTY){
-                    newBoard[r][c] = PIECES.EMPTY;
+    for (let i = 0; i < moveSequence.length - 1; i++) {
+        const from = moveSequence[i];
+        const to = moveSequence[i + 1];
+        if (Math.abs(from[0] - to[0]) > 1) {
+            const dr = Math.sign(to[0] - from[0]);
+            const dc = Math.sign(to[1] - from[1]);
+            let cr = from[0], cc = from[1];
+            while(cr !== to[0] || cc !== to[1]) {
+                 if (newBoard[cr][cc] !== PIECES.EMPTY && !(cr === from[0] && cc === from[1])) {
+                    newBoard[cr][cc] = PIECES.EMPTY;
                     break;
                 }
-                r += dr;
-                c += dc;
+                cr += dr; cc += dc;
             }
         }
     }
 
+    if ((isP1(piece) && endPos[0] === 0) || (isP2(piece) && endPos[0] === 7)) {
+        piece = isP1(piece) ? PIECES.P1_KING : PIECES.P2_KING;
+    }
+    
     newBoard[endPos[0]][endPos[1]] = piece;
-
-    const player = (piece === PIECES.P1_PAWN || piece === PIECES.P1_KING) ? 1 : 2;
-    if ((player === 1 && endPos[0] === 0 && piece === PIECES.P1_PAWN)) {
-        newBoard[endPos[0]][endPos[1]] = PIECES.P1_KING;
-    }
-    if ((player === 2 && endPos[0] === 7 && piece === PIECES.P2_PAWN)) {
-        newBoard[endPos[0]][endPos[1]] = PIECES.P2_KING;
-    }
     
     return newBoard;
 };
@@ -205,8 +147,8 @@ const checkGameState = (board, nextPlayer) => {
 
     for (let r = 0; r < 8; r++) {
         for (let c = 0; c < 8; c++) {
-            if (isPlayerPiece(board[r][c], 1)) p1Pieces++;
-            if (isPlayerPiece(board[r][c], 2)) p2Pieces++;
+            if (isP1(board[r][c])) p1Pieces++;
+            if (isP2(board[r][c])) p2Pieces++;
         }
     }
 
@@ -226,6 +168,5 @@ module.exports = {
     createInitialBoard,
     getValidMoves,
     applyMove,
-    checkGameState,
-    isPlayerPiece
+    checkGameState
 };
